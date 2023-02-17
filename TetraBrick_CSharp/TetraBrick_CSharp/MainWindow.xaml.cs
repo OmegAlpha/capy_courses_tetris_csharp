7bbabb9486aa8e5b84844ed2cadbca120f04aaac
omegalpha <ariellarias@gmail.com>
2023-02-17 17:13:43 +1300

    class 03

--- TetraBrick_CSharp/TetraBrick_CSharp/MainWindow.xaml.cs
+++ TetraBrick_CSharp/TetraBrick_CSharp/MainWindow.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace TetraBrick_CSharp
 {
     public partial class MainWindow
     {
         public TetraGame game;
-        
+
         public MainWindow()
         {
             InitializeComponent();
 
-            game = new TetraGame();
+            game = new TetraGame(this);
+            
+            game.StartEngine();
         }
     }
 }
--- TetraBrick_CSharp/TetraBrick_CSharp/Tetra/TetraGame.cs
+++ TetraBrick_CSharp/TetraBrick_CSharp/Tetra/TetraGame.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using TetraBrick_CSharp;
 
 public class TetraGame
 {
       public const int FRAME_TIME = 16;
+      public const int CELL_SIZE = 30;
+      public const int GRID_HEIGHT = 20;
+      public const int GRID_WIDTH = 10;
       
       // Grilla de fondo 
       // Piezas
@@ -11,12 +17,43 @@ public class TetraGame
       // timer 
       // Sonido
       // Graficos
+
+      private MainWindow _mainWindow;
       
-      public TetraGame()
+      public TetraGame(MainWindow mainWindow)
       {
             Console.WriteLine("Creating Tetra Game");
+            _mainWindow = mainWindow;
+            
+            Initialize();
+      }
 
+      public void StartEngine()
+      {
+            Console.WriteLine("Tetra Game Starting Game");
+            
             GameLoop();
+      }
+
+      private void Initialize()
+      {
+            Console.WriteLine("Initializing Tetra Game");
+            
+            for (int y = 0; y < GRID_HEIGHT; y++)
+            {
+                  for (int x = 0; x < GRID_WIDTH; x++)
+                  { 
+                        Image imgBGCell = new Image();
+                        imgBGCell.Width = CELL_SIZE;
+                        imgBGCell.Height = CELL_SIZE;
+                        imgBGCell.Source = new BitmapImage(new Uri("Assets/woooo.png", UriKind.Relative ));
+
+                        _mainWindow.GameCanvas.Children.Add(imgBGCell);
+                  
+                        Canvas.SetLeft(imgBGCell, x * CELL_SIZE);
+                        Canvas.SetTop(imgBGCell, y * CELL_SIZE);
+                  }     
+            }
       }
 
       private async Task GameLoop()
